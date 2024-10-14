226f2307c726277ec54dc12117b0a4a4be2b6501
yashpatel-2964 <147785353+yashpatel-2964@users.noreply.github.com>
2024-10-14 23:31:06 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcEfDemo.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public string? ProductCategory { get; set; }
+
+    public int? ProductPrice { get; set; }
+}
---
+++ ProductDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcEfDemo.Models;
+
+public partial class ProductDbContext : DbContext
+{
+    public ProductDbContext()
+    {
+    }
+
+    public ProductDbContext(DbContextOptions<ProductDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=KIRAN_BARAIYA; Database=ProductDb; Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("Product");
+
+            entity.Property(e => e.ProductCategory).HasMaxLength(200);
+            entity.Property(e => e.ProductName).HasMaxLength(200);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
